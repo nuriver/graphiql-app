85273fce3074e394bc3f51583b2534fa78f79aec
Aleksei Golubkov <nuriver@gmail.com>
2024-08-27 20:53:55 +0200

    feat: add store provider

--- src/app/graphiql-client/Graphiql/SdlInput.tsx
+++ src/app/graphiql-client/Graphiql/SdlInput.tsx
@@ -2,7 +2,7 @@ export default function SdlInput(): JSX.Element {
   return (
     <div className="graphiql-endpoint-wrapper">
       <label className="graphiql-endpoint-label" htmlFor="graphiql-sdl-input">
-        Endpoint URL
+        SDL URL
       </label>
       <input
         type="text"
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,47 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { useDispatch, useSelector, useStore } from 'react-redux';
 import headersReducer from './headersSlice';
-import type { HeadersState } from './headersSlice';
 
-const store = configureStore({
-  reducer: {
-    headers: headersReducer,
-  },
+const rootReducer = combineReducers({
+  headers: headersReducer,
 });
 
-export type RootState = {
-  headers: HeadersState;
+export const setupStore = (preloadedState?: Partial<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
 };
-export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    devTools: true,
+  });
+};
+
+export type AppState = ReturnType<AppStore['getState']>;
+export type AppStore = ReturnType<typeof makeStore>;
+export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<typeof rootReducer>;
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppStore = useStore.withTypes<AppStore>();
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import headersReducer from './headersSlice';
+// import type { HeadersState } from './headersSlice';
+
+// const store = configureStore({
+//   reducer: {
+//     headers: headersReducer,
+//   },
+// });
+
+// export type RootState = {
+//   headers: HeadersState;
+// };
+// export type AppDispatch = typeof store.dispatch;
+
+// export default store;
