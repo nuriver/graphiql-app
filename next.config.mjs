f30c122b53fe69e3a75ced8ca25c8b310a7eecd3
Maryinfun <motmaryion@gmail.com>
2024-09-02 12:57:26 +0300

    fix: correct i18 config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
-import { i18n } from './next-i18next.config.js';
+import { i18n } from './next-i18next.config.mjs';
+
 const nextConfig = {
   distDir: './dist',
   i18n,
 };
-
 export default nextConfig;
