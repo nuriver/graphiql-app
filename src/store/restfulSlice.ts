8ee8ea539e4d3be761fe701fae990dae68d92b44
Aleksei Golubkov <nuriver@gmail.com>
2024-09-15 10:37:50 +0200

    fix: fix loader

--- src/store/restfulSlice.ts
+++ src/store/restfulSlice.ts
@@ -47,7 +47,6 @@ const RestfulSlice = createSlice({
       state.variables = action.payload || '{}';
     },
     setRestfulUrl(state, action: PayloadAction<string>) {
-      console.log('Adding URL to restfulUrls:', action.payload);
       state.url = action.payload;
     },
     resetRestfulStore() {
@@ -78,88 +77,3 @@ export const {
 } = RestfulSlice.actions;
 
 export default RestfulSlice.reducer;
-// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-// import { RestfulState, ResponseState } from '../core/types';
-
-// export interface RestfulHeader {
-//   headerKey: string;
-//   headerValue: string;
-// }
-
-// export interface HeadersState {
-//   headers: RestfulHeader[];
-// }
-
-// const initialState: RestfulState = {
-//   method: 'GET',
-//   headers: [],
-//   endpoint: '',
-//   body: '',
-//   variables: '',
-//   url: '',
-// };
-
-// const RestfulSlice = createSlice({
-//   name: 'RestfulSlice',
-//   initialState,
-//   reducers: {
-//     addHeader(state, action) {
-//       state.headers.push(action.payload);
-//     },
-//     removeHeader(state, action: PayloadAction<number>) {
-//       state.headers = state.headers.filter(
-//         (_, index) => index !== action.payload
-//       );
-//     },
-//     setRestfulMethod: (state, action) => {
-//       state.method = action.payload;
-//     },
-//     setRestfulEndpoint: (state, action: PayloadAction<string>) => {
-//       state.endpoint = action.payload;
-//     },
-//     setRestfulBody: (state, action) => {
-//       state.body = action.payload;
-//     },
-//     setRestfulVariables: (state, action) => {
-//       state.variables = action.payload;
-//       if (action.payload === '') {
-//         state.variables = '{}';
-//       }
-//     },
-
-//     setRestfulUrl: (state, action) => {
-//       console.log('Adding URL to restfulUrls:', action.payload);
-//       state.url = action.payload;
-//     },
-//     resetRestfulStore: () => {
-//       return {
-//         method: 'GET',
-//         endpoint: '',
-//         body: '',
-//         variables: '',
-//         headers: [],
-//         url: '',
-//       };
-//     },
-//     setRestfulStore: (state, action: PayloadAction<RestfulState>) => {
-//       return action.payload;
-//     },
-//     setResponse(state, action: PayloadAction<ResponseState>) {
-//       state.response = action.payload;
-//     },
-//   },
-// });
-
-// export const {
-//   addHeader,
-//   removeHeader,
-//   setRestfulMethod,
-//   setRestfulEndpoint,
-//   setRestfulBody,
-//   setRestfulVariables,
-//   setRestfulUrl,
-//   resetRestfulStore,
-//   setRestfulStore,
-//   setResponse
-// } = RestfulSlice.actions;
-// export default RestfulSlice.reducer;
